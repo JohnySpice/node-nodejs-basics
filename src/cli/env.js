85764045a7d0b2e3ac0d98a609fef0e5c1de8115
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-03 12:49:19 +0300

    feat: add parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,7 @@
 export const parseEnv = () => {
-    // Write your code here 
+    for (const [variableName, variableValue] of Object.entries(process.env)) {
+        if (variableName.match(/^rss_/i)) {
+            console.log(`${variableName}=${variableValue};`);
+        }
+    }
 };
