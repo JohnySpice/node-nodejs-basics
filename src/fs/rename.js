bb66ab27e58c8bfd6274c39947e9015a93ded46b
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-03 11:36:29 +0300

    feat: add rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,11 @@
+import {resolve} from 'path';
+import {readdir, rename as rn} from 'fs/promises'
+
 export const rename = async () => {
-    // Write your code here 
+    const filesDirPath = 'src/fs/files';
+    const filesNames = await readdir(filesDirPath);
+    if (!filesNames.includes('wrongFilename.txt') || filesNames.includes('properFilename.md')) {
+        throw new Error('FS operation failed');
+    }
+    await rn(resolve(filesDirPath, 'wrongFilename.txt'), resolve(filesDirPath, 'properFilename.md'));
 };
