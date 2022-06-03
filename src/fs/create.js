bf7e92ebd232e09da3dcb0b0b4c73a32e760fa8d
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-03 10:55:15 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,13 @@
+import {access, writeFile} from 'fs/promises';
+import {resolve} from "path";
+
 export const create = async () => {
-    // Write your code here 
-};
+    const filePath = resolve('src/fs/files/fresh.txt');
+    try {
+        await access(filePath);
+    } catch (e) {
+        const string = new Uint8Array(Buffer.from('I am fresh and young'));
+        return await writeFile(filePath, string);
+    }
+    throw new Error('FS operation failed');
+}
