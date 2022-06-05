d834333359a79997d48c982037a8483e55223f01
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-05 16:46:22 +0300

    feat: add functions for working with Worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,25 @@
+import {cpus} from 'os';
+import {dirname, resolve} from 'path';
+import {Worker} from 'worker_threads';
+
 export const performCalculations = async () => {
-    // Write your code here
+    const cpuInfo = cpus();
+    const workerPool = [];
+    for (let i = 0; i < cpuInfo.length; i++) {
+        workerPool.push(createWorker(i));
+    }
+    return Promise.all(workerPool);
 };
+
+async function createWorker(data) {
+    return new Promise(res => {
+        const currentFolderPath = dirname(new URL(import.meta.url).pathname);
+        const worker = new Worker(resolve(currentFolderPath, 'worker.js'), {
+            workerData: data + 10,
+        });
+        worker.on('message', m => res({status: 'resolved', data: m}));
+        worker.on('error', () => res({status: 'error', value: null}));
+    });
+}
+
+performCalculations().then(result => console.log(result));
