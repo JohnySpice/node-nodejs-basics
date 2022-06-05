7d5079c747cc78936f35a7536004346f2303c0e8
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-05 14:08:47 +0300

    feat: add streams/read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,11 @@
+import {dirname, resolve} from 'path';
+import {createReadStream} from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    const currentFolderPath = dirname(new URL(import.meta.url).pathname);
+    const fileToReadPath = resolve(currentFolderPath, 'files', 'fileToRead.txt');
+    const rs = createReadStream(fileToReadPath);
+    rs.pipe(process.stdout);
 };
+
+read()
