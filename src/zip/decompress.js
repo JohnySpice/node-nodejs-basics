afa087705e9467084c0bd2c801452082e1db7228
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-05 14:30:31 +0300

    feat: add zip/decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,18 @@
+import {createUnzip} from 'zlib';
+import {dirname, resolve} from 'path';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream';
+
 export const decompress = async () => {
-    // Write your code here 
+    const unzip = createUnzip();
+    const currentFolderPath = dirname(new URL(import.meta.url).pathname);
+    const archive = createReadStream(resolve(currentFolderPath, 'files', 'archive.gz'));
+    const decompressedFile = await createWriteStream(resolve(currentFolderPath, 'files', 'fileToCompress.txt'));
+    pipeline(archive, unzip, decompressedFile, err => {
+        if (err) {
+            console.error(err);
+        }
+    }); 
 };
+
+decompress();
