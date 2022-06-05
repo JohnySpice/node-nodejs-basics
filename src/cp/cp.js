8f0dbb70ce636637532fe7dd984dbb60fec84fc8
JohnySpice <tyli008@mail.ru>
2022-06-05 18:48:11 +0300

    feat: add spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,11 @@
+import {fork} from 'child_process';
+import {dirname, resolve} from 'path';
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    const currentFolderPath = dirname(new URL(import.meta.url).pathname);
+    fork(resolve(currentFolderPath, 'files', 'script.js'), args, {
+        stdio: 'inherit',
+    });
 };
+
+spawnChildProcess(['First Arg', 'Second Arg', 'Third Arg', 'etc Arg']);
