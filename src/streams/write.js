6ec18464e9302bf853fb2e8d05390a8c8933bfd0
JohnySpice <tyli008@mail.ru>
2022-06-05 14:09:12 +0300

    feat: add streams/write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,15 @@
+import {dirname, resolve} from 'path';
+import {createWriteStream} from 'fs';
+
 export const write = async () => {
-    // Write your code here 
+    const currentFolderPath = dirname(new URL(import.meta.url).pathname);
+    const fileToReadPath = resolve(currentFolderPath, 'files', 'fileToWrite.txt');
+    const ws = createWriteStream(fileToReadPath);
+    process.stdin.pipe(ws);
 };
+
+write()
+/*
+write.js - implement function that 
+writes process.stdin data into file fileToWrite.txt content using Writable Stream
+ */
