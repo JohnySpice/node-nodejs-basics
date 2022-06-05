42edc63154d09c91ac79d63be62552058f251aa3
Viacheslav Tiulkov <tulkov@trend.tech>
2022-06-05 14:09:37 +0300

    feat: add streams/transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,21 @@
+import {Transform, pipeline} from 'stream';
+
 export const transform = async () => {
-    // Write your code here 
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        }
+    });
+    pipeline(
+        process.stdin,
+        reverse,
+        process.stdout,
+        err => {
+            if (err) {
+                console.error(err.message);
+            }
+        }
+    );
 };
+
+transform();
